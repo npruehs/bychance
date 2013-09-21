a76fe72e71d3c7569a616fb74d4ba93e168cceda
Nick Pruehs <dev@npruehs.de>
2013-09-21 09:06:38 +0200

    CHANGED: License - Put ByChance under the MIT license.

--- Source/ByChanceFramework/Configuration/PostProcessing/DiscardOpenContextsPolicy.cs
+++ Source/ByChanceFramework/Configuration/PostProcessing/DiscardOpenContextsPolicy.cs
@@ -1,22 +1,6 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
 // <copyright file="DiscardOpenContextsPolicy.cs" company="Nick Pruehs, Denis Vaz Alves">
 //   Copyright 2011-2013 Nick Pruehs, Denis Vaz Alves.
-//   
-//   This file is part of the ByChance Framework.
-//   
-//   The ByChance Framework is free software: you can redistribute it and/or
-//   modify it under the terms of the GNU Lesser General Public License as
-//   published by the Free Software Foundation, either version 3 of the License,
-//   or (at your option) any later version.
-//   
-//   The ByChance Framework is distributed in the hope that it will be useful,
-//   but WITHOUT ANY WARRANTY; without even the implied warranty of
-//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//   GNU Lesser General Public License for more details.
-//   
-//   You should have received a copy of the GNU Lesser General Public License
-//   along with the ByChance Framework.  If not, see
-//   <http://www.gnu.org/licenses/>.
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
 namespace ByChance.Configuration.PostProcessing
--- Source/ByChanceFramework/Configuration/PostProcessing/PostProcessingPolicy.cs
+++ Source/ByChanceFramework/Configuration/PostProcessing/PostProcessingPolicy.cs
@@ -1,22 +1,6 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
 // <copyright file="PostProcessingPolicy.cs" company="Nick Pruehs, Denis Vaz Alves">
 //   Copyright 2011-2013 Nick Pruehs, Denis Vaz Alves.
-//   
-//   This file is part of the ByChance Framework.
-//   
-//   The ByChance Framework is free software: you can redistribute it and/or
-//   modify it under the terms of the GNU Lesser General Public License as
-//   published by the Free Software Foundation, either version 3 of the License,
-//   or (at your option) any later version.
-//   
-//   The ByChance Framework is distributed in the hope that it will be useful,
-//   but WITHOUT ANY WARRANTY; without even the implied warranty of
-//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//   GNU Lesser General Public License for more details.
-//   
-//   You should have received a copy of the GNU Lesser General Public License
-//   along with the ByChance Framework.  If not, see
-//   <http://www.gnu.org/licenses/>.
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
 namespace ByChance.Configuration.PostProcessing
@@ -69,7 +53,8 @@ namespace ByChance.Configuration.PostProcessing
         /// <param name="levelGeneratorConfiguration">Configuration of the level generator that built the level to be processed.</param>
         /// <param name="level">Level to process.</param>
         /// <exception cref="ArgumentNullException"><paramref name="levelGeneratorConfiguration"/> or <paramref name="level"/> is <c>null</c>.</exception>
-        public virtual void Process<T>(LevelGeneratorConfiguration levelGeneratorConfiguration, Level<T> level) where T : Chunk
+        public virtual void Process<T>(LevelGeneratorConfiguration levelGeneratorConfiguration, Level<T> level)
+            where T : Chunk
         {
             if (levelGeneratorConfiguration == null)
             {
